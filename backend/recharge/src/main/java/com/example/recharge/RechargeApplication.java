f284a224f1e2ad2476ddf525505565ec175a9e0d
sainarendar23 <104960622+sainarendar23@users.noreply.github.com>
2024-03-26 10:19:53 +0530

    Add files via upload

---
+++ backend/recharge/src/main/java/com/example/recharge/RechargeApplication.java
@@ -0,0 +1,13 @@
+package com.example.recharge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RechargeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RechargeApplication.class, args);
+	}
+
+}
